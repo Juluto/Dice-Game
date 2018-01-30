d1c99c54d7f1ee41511ae76c440e00d1813e8f4e
Juluto <julian.mayer94@gmail.com>
2018-01-30 21:07:15 +0100

    maj script

--- Assets/cubeScript.cs
+++ Assets/cubeScript.cs
@@ -8,7 +8,7 @@ public class cubeScript : MonoBehaviour
     public int face;
     static Rigidbody rb;
     public static Vector3 diceVelocity;
-    public Text dice;
+    public Text Dice;
     bool playdice = false;
 
     // Use this for initialization
@@ -41,10 +41,10 @@ public class cubeScript : MonoBehaviour
     // Afficher la face du dé
     IEnumerator ShowFace()
     {
-        dice.enabled = true;
-        dice.GetComponent<Text>().text = face.ToString();
+        Dice.enabled = true;
+        Dice.GetComponent<Text>().text = face.ToString();
         yield return new WaitForSeconds(2f);
-        dice.enabled = false;
+        Dice.enabled = false;
     }
 
     // Lancer le dé
